10de6109b91f769c7ed14d2c291dd77d73960d78
gunjubasich <gunjubasich@gmail.com>
2017-03-11 23:49:42 +0500

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function(){
 
+	$(".portfolio_item").each(function(i) {
+	$(this).find("a").attr("href", "#work_" + i);
+	$(this).find(".pot_descr").attr("id", "work_" + i);
+	});
+
 	var mixer = mixitup("#portfolio_grid");
 
 	$(".s_portfolio li").click(function(){
@@ -9,6 +14,7 @@ $(document).ready(function(){
 	});
 
 	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({type:"inline",midClick: true});
 
 	$(".top_descr h1").animated("zoomInUp", "zoomOutDown");
 	$(".top_descr p, .section_header").animated("fadeInUp", "FadeOutDown");
